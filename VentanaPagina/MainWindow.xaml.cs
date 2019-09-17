7794b89b5f1a231d6ca6d1aeb482fbe2f2509aba
unknown <trabajodam18@gmail.com>
2019-09-17 12:53:14 +0200

    Crear Aplicaciones Wpf con Window o Page

---
+++ VentanaPagina/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VentanaPagina
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Ventana v = new Ventana();
+            v.Show();
+        }
+    }
+}
---
+++ VentanaPagina/Pagina.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VentanaPagina
+{
+    /// <summary>
+    /// Lógica de interacción para Pagina.xaml
+    /// </summary>
+    public partial class Pagina : Page
+    {
+        public Pagina()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Ventana v = new Ventana();
+            v.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            PaginaSiguiente mipagina = new PaginaSiguiente();
+            this.NavigationService.Navigate(mipagina);
+        }
+    }
+}
